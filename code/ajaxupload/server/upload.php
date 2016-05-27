374e2ad4a17785bd7a12b4e38909599a1bde44d3
chenjunwei <1045136563@qq.com>
2016-05-27 14:38:03 +0800

    修改上传路径，格式化代码

--- code/ajaxupload/server/upload.php
+++ code/ajaxupload/server/upload.php
@@ -1,13 +1,13 @@
 <?php
 require(dirname(__FILE__) . '/include/Uploader.php');   // 引入所需要的文件上传类文件
 
-$upload_dir = '/upload_files/';                         // 设置服务器文件上传目录
+$upload_dir = dirname(__FILE__) . '/upload_files/';     // 设置服务器文件上传目录
 
 $uploader = new FileUpload('uploadfile');
-$result = $uploader->handleUpload($upload_dir);
+$result   = $uploader->handleUpload($upload_dir);
 
 if (!$result) {
-  exit(json_encode(array('success' => false, 'msg' => $uploader->getErrorMsg())));
+    exit(json_encode(array('success' => false, 'msg' => $uploader->getErrorMsg())));
 }
 
 echo json_encode(array('success' => true));
